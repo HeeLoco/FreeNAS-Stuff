9778451fb3bbc65c642df41a8cb0497ea749fb74
HeeLoco <66466597+HeeLoco@users.noreply.github.com>
2020-06-24 14:10:33 +0200

    Create unlock-volumeXY-withPass.py

---
+++ unlock-volumeXY-withPass.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+req = requests.post(
+        'http://localhost/api/v1.0/storage/volume/Pool/unlock/',
+        auth=('root', 'PASSWORD'),
+        headers={'Content-Type': 'application/json'},
+        verify=False,
+
+        data=json.dumps({
+                'passphrase': 'PASSPHRASE'
+        }),
+)
+print (req.text)
